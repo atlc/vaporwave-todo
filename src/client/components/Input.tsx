f9e4a0e8c3d3dfe1720b74dc942d02d9307a3117
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2023-03-08 22:24:38 -0600

    Reset input state upon successful submission and allow return key to trigger submissions

--- src/client/components/Input.tsx
+++ src/client/components/Input.tsx
@@ -6,14 +6,23 @@ const Input = ({ reload }: { reload: () => void }) => {
 
     const handleSubmit = () => {
         POST("/api/items", { content: item })
-            .then(reload)
+            .then(() => {
+                setItem("");
+                reload();
+            })
             .catch(err => console.error(err));
+    };
+
+    const submitOnReturn = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            handleSubmit();
+        }
     };
 
     return (
         <div className="border border-secondary border-2 row justify-content-end p-3 mb-4 bg-light rounded-3 shadow justify-content-center">
             <div className="col-12 col-md-11">
-                <input className="my-2 text-info form-control" placeholder='"Make todo list"' type="text" value={item} onChange={e => setItem(e.target.value)} />
+                <input onKeyDown={submitOnReturn} className="my-2 text-info form-control" placeholder='"Make todo list"' type="text" value={item} onChange={e => setItem(e.target.value)} />
             </div>
             <div className="col-12 col-md-1">
                 <button onClick={handleSubmit} className="my-2 btn btn-info">
